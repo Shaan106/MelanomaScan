4c30a282c04c5257bd70d381430dfa197ece6da3
Aasmaan Yadav <aasmaanyadav1@gmail.com>
2021-09-18 12:19:12 +0100

    Created project

---
+++ MelanomaScan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MelanomaScan
+//
+//  Created by Yadav, Aasmaan (SPH) on 18/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MelanomaScan/MelanomaScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelanomaScanApp.swift
+//  MelanomaScan
+//
+//  Created by Yadav, Aasmaan (SPH) on 18/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MelanomaScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
