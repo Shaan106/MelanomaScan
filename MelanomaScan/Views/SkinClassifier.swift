5a2f3c2636676d019363bb02d65be26faee60cfe
Shaan <shaan@yadu.in>
2021-10-15 09:16:23 +0100

    added a picker to allow for image selection for the images to be classified

--- MelanomaScan/Views/SkinClassifier.swift
+++ MelanomaScan/Views/SkinClassifier.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SkinClassifier: View {
     
     //display variables to do with the output of image classification
+    private var listOfImages = ["testImageMalignant1","testImageMalignant2","testImageMalignant3","testImageBenign1","testImageBenign2","testImageBenign3"]
     @State private var currentImageName = "testImageMalignant1"
     @State private var classificationLabel: String = ""
     @State private var confidence: Double = 0
@@ -23,10 +24,18 @@ struct SkinClassifier: View {
         
         VStack {
             
+            //picker to select image for classification
+            Picker("", selection: $currentImageName) {
+                            ForEach(listOfImages, id: \.self) {
+                                Text($0)
+                            }
+                        }
+            .padding()
+            
             //displaying chosen image, as well as information about classification of that image
             Image(currentImageName)
                 .resizable()
-//                .frame(width:UIScreen.main.bounds.width*(3/4), height:UIScreen.main.bounds.height*(1/4))
+                //.frame(width:UIScreen.main.bounds.width*(3/4), height:UIScreen.main.bounds.height*(1/4))
                 .frame(width:200,height:200)
             
             //button to call subroutine to classify image
@@ -34,19 +43,19 @@ struct SkinClassifier: View {
                 (self.classificationLabel, self.confidence) = imageClassifierInstance.performImageClassification(imageName: currentImageName)
             }
             
+            //what the image is classified as
             Text(classificationLabel)
                 .padding()
-                .font(.largeTitle)
+                .font(.title)
             
+            //confidence of classification
             Text(String(confidence))
                 .padding()
-                .font(.largeTitle)
+                .font(.title)
             
             NavigationLink("To Camera (choose new img)", destination: Camera())
-                .padding()
             
             NavigationLink("To Information Page (change risk profile)", destination: InformationPage())
-                .padding()
         }
     }
     
