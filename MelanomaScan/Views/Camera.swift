46b709e89480f4e9ffd50d28ce959841cf4b374d
Aasmaan Yadav <aasmaanyadav1@gmail.com>
2021-11-25 11:13:59 +0000

    Added ability to open camera from inside app. Camera only opens, does not return anything as of now.

--- MelanomaScan/Views/Camera.swift
+++ MelanomaScan/Views/Camera.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct Camera: View {
+    
+    @State var openCamera = false
+    @State var shownImage = UIImage()
+    
     var body: some View {
-        Text("Camera page")
-        .navigationBarTitle("Camera")
+        
+        VStack {
+            Text("Camera page")
+            .navigationBarTitle("Camera")
+            
+            Button("Open Camera") {
+                openCamera = true
+            }
+            
+        }.sheet(isPresented: $openCamera, content: {
+            ImagePicker(selectedImage: $shownImage, sourceType: .camera)
+        })
     }
 }
 
