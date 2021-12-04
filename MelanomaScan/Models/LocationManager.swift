78478d2e5aa9d88865158c1de2be2668ca17c40d
Shaan <shaan@yadu.in>
2021-12-04 23:42:40 +0000

    Finished ticket 4 sprint 8. Added map, and co-ordinates.

--- MelanomaScan/Models/LocationManager.swift
+++ MelanomaScan/Models/LocationManager.swift
@@ -5,4 +5,54 @@
 //  Created by Aasmaan Yadav on 04/12/21.
 //
 
+import Foundation
+import CoreLocation
+import Combine
 
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    //setting up everything required from CoreLocation library
+    //set up as published, so functions do not need to return values
+    private let locationManager = CLLocationManager()
+    @Published var locationStatus: CLAuthorizationStatus?
+    @Published var lastLocation: CLLocation?
+    
+    //required setting up of CoreLocation instance
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    //checks whether user has allowed access to the location services
+    //if the user has not given acess, returns what access given
+    var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+    }
+    
+    //updating the location status
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationStatus = status
+        //print(#function, statusString)
+    }
+    
+    //updating the current location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        lastLocation = location
+        //print(#function, location)
+    }
+    
+}
--- MelanomaScan/Views/WeatherRisks.swift
+++ MelanomaScan/Views/WeatherRisks.swift
@@ -6,22 +6,49 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct WeatherRisks: View {
     
     let uvIndexManager = UVIndexManager()
     
+    @StateObject var locationManager = LocationManager()
+    
+    //setting the user's co-ordinates
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    //using apple's Mapkit to create an initial region, completely random
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 1, longitude: 1), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    
     var body: some View {
         
         VStack {
             
-            Button("Test Request") {
-                uvIndexManager.testRequest(callback: {(uvResponse: Double, uvMaxResponse: Double, ozoneResponse: Double) -> () in
-                    print("UV Index: " + String(uvResponse))
-                    print("Max UV Index: " + String(uvMaxResponse))
-                    print("Ozone levels: " + String(ozoneResponse))
-                } )
+            Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+                        .frame(width: 400, height: 300)
+            
+            Spacer()
+            
+            VStack {
+                
+                Text("location status: \(locationManager.statusString)")
+                Text("latitude: \(userLatitude)")
+                Text("longitude: \(userLongitude)")
+                
+                Button("Test Request") {
+                    uvIndexManager.testRequest(callback: {(uvResponse: Double, uvMaxResponse: Double, ozoneResponse: Double) -> () in
+                        print("UV Index: " + String(uvResponse))
+                        print("Max UV Index: " + String(uvMaxResponse))
+                        print("Ozone levels: " + String(ozoneResponse))
+                    } )
+                }
             }
+            Spacer()
             
         }
         .navigationBarTitle("Weather Risks")
