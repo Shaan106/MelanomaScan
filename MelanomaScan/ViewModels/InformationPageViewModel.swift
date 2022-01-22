38e7613842245dbdf34f420c1131f4db3bae0da7
Shaan <shaan@yadu.in>
2022-01-22 12:13:08 +0000

    Finished development section of the app

--- MelanomaScan/ViewModels/InformationPageViewModel.swift
+++ MelanomaScan/ViewModels/InformationPageViewModel.swift
@@ -14,39 +14,108 @@ class InformationPageViewModel {
         
         if riskFactorVal == -1 {
             
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.white)
-                .frame(width: UIScreen.screenWidth/6, height: 20)
+            VStack {
+                Text("n/a").padding()
+                HStack {
+                    Text("Risk: ").bold()
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 0 / 255, green: 255 / 255, blue: 128 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 192 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 245 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color.orange.opacity(0.5))
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                }
+            }.padding(.bottom)
             
-        } else if riskFactorVal < 3 {
+        } else if riskFactorVal <= 1.6 {
             
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.green)
-                .frame(width: UIScreen.screenWidth/6, height: 20)
+            VStack {
+                Text("You should check your skin every 6 months").padding()
+                HStack {
+                    Text("Risk: ").bold()
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 0 / 255, green: 255 / 255, blue: 128 / 255)).opacity(1)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 192 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 245 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color.orange.opacity(0.5))
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                }
+            }.padding(.bottom)
             
-        } else if riskFactorVal < 6 {
+        } else if riskFactorVal <= 2.5 {
             
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.yellow)
-                .frame(width: UIScreen.screenWidth/6, height: 20)
+            VStack {
+                Text("You should check your skin every 3-6 months").padding()
+                HStack {
+                    Text("Risk: ").bold()
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 0 / 255, green: 255 / 255, blue: 128 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 192 / 255, green: 255 / 255, blue: 0 / 255)).opacity(1)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 245 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color.orange.opacity(0.5))
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                }
+            }.padding(.bottom)
             
-        } else if riskFactorVal < 8 {
+        } else if riskFactorVal <= 5 {
             
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.orange)
-                .frame(width: UIScreen.screenWidth/6, height: 20)
+            VStack {
+                Text("You should check your skin every 2-3 months").padding()
+                HStack {
+                    Text("Risk: ").bold()
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 0 / 255, green: 255 / 255, blue: 128 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 192 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 245 / 255, green: 255 / 255, blue: 0 / 255)).opacity(1)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color.orange.opacity(0.5))
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                }
+            }.padding(.bottom)
             
-        } else if riskFactorVal < 11 {
+        } else if riskFactorVal > 5 {
             
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.red)
-                .frame(width: UIScreen.screenWidth/6, height: 20)
-            
-        } else if riskFactorVal >= 11 {
-            
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.purple)
-                .frame(width: UIScreen.screenWidth/6, height: 20)
+            VStack {
+                Text("You should check with a GP about the information you entered").padding()
+                HStack {
+                    Text("Risk: ").bold()
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 0 / 255, green: 255 / 255, blue: 128 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 192 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color(red: 245 / 255, green: 255 / 255, blue: 0 / 255)).opacity(0.5)
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color.orange.opacity(1))
+                        .frame(width: UIScreen.screenWidth/6, height: 20)
+                }
+            }.padding(.bottom)
             
         }
         
--- MelanomaScan/Views/InformationPage.swift
+++ MelanomaScan/Views/InformationPage.swift
@@ -13,8 +13,10 @@ struct InformationPage: View {
     @State private var riskFactorValue: String = "" //convert to other data types later
     @State private var needsRefresh: Bool = false
     @State private var riskFactorsList: [RiskFactors] = [RiskFactors]()
+    @State private var showingResetRiskFactorsAlert = false
     
     let riskFactorsModel = RiskFactorsModel()
+    let informationPageViewModel = InformationPageViewModel()
     let stringListOfRiskFactors = ["Age", "Gender", "Skin Type", "Eye Colour", "Hair Colour", "Number of Moles", "Freckles", "Family History", "Diseases and Conditions", "Body Mass Index"]
     
     //again into a VM model
@@ -50,14 +52,26 @@ struct InformationPage: View {
                 .accentColor(needsRefresh ? .black: .white)
                 
                 Button("Reset All") {
-                    // coreDataManager.saveRiskFactorValue(stringValue: riskFactorValue)
-                    riskFactorsModel.resetRiskFactors(coreDataManager: coreDataManager, listOfRiskFactors: stringListOfRiskFactors)
-                    populateRiskFactors()
-                    //needsRefresh.toggle()
+                    showingResetRiskFactorsAlert = true
                 }.buttonStyle(NeumorphicButtonStyle(color: Color("Background")))
                     .padding()
+                    .alert(isPresented: $showingResetRiskFactorsAlert) {
+                        Alert(
+                            title: Text("Are you sure you want to reset everything?"),
+                            message: Text(""),
+                            primaryButton: .destructive(Text("Reset")) {
+                                riskFactorsModel.resetRiskFactors(coreDataManager: coreDataManager, listOfRiskFactors: stringListOfRiskFactors)
+                                populateRiskFactors()
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    }
                 
-                Text( riskFactorsModel.calculateFinalRiskFactor(riskFactorsList: riskFactorsList) ).padding()
+                //text showing numerical risk factor value
+                //Text( riskFactorsModel.calculateFinalRiskFactor(riskFactorsList: riskFactorsList) ).padding()
+                informationPageViewModel.getRiskFactorColourCodedRectangles(riskFactorVal: Double(riskFactorsModel.calculateFinalRiskFactor(riskFactorsList: riskFactorsList)) ?? -1 )
+                
+                
                 
             }.navigationBarTitle("Information Page")
                 .background(Color("Background"))
